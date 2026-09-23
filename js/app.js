dc02743b65f5bb2dae4be0660d80bc52d932cd5b
Jeff <jepalen@gmail.com>
2026-09-23 12:03:04 +0200

    better texts

--- js/app.js
+++ js/app.js
@@ -263,7 +263,7 @@ document.addEventListener('alpine:init', () => {
                             price: data.price?.value || 0,
                             currency: "€",
                             period: "mes",
-                            status: this._capitalize(data.status?.trim()) || 'Consultar disponibilidad',
+                            status: this._capitalize(data.status?.trim()) || 'Contactar',
                             description: this._capitalize(data.description) || '',
                             commodities: this._buildCommodities(data, meta)
                         };
